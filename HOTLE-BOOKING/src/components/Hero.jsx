3b98932fab76ad8c0a05404fd272e54946c8995e
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-03 21:00:53 +0500

    Initial commit

---
+++ HOTLE-BOOKING/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-start justify-center px-6 md:px-32 text-white bg-[ur("/src/assets/heroImage.png")]'>
+
+    </div>
+  )
+}
+
+export default Hero
