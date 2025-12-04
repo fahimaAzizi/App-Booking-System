1b93afd5768db9edfec39f3eb3b109fc1e2b053b
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-05 00:08:20 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/Navbar.jsx
+++ HOTLE-BOOKING/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const App = () => {
                     </button>
                 </div>
 
-                {/* Desktop Right */}
+                {/* Desktop Right */}  
                 <div className="hidden md:flex items-center gap-4">
                 
                    <image src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
@@ -57,16 +57,13 @@ const App = () => {
 
                 {/* Mobile Menu Button */}
                 <div className="flex items-center gap-3 md:hidden">
-                    <image src={assets.closeIcon} alt=" close-menu"/>
+                    <image src={assets.menuIcon} alt=" " className={`${isScrolled && "invert"} h-4`}/>
                 </div>
 
                 {/* Mobile Menu */}
                 <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-                        <svg className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                            <line x1="18" y1="6" x2="6" y2="18" />
-                            <line x1="6" y1="6" x2="18" y2="18" />
-                        </svg>
+                      
                     </button>
 
                     {navLinks.map((link, i) => (
