96990e2d3fb3a22225b196bfa36c1769a976eeeb
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-07 21:33:12 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/Hero.jsx
+++ HOTLE-BOOKING/src/components/Hero.jsx
@@ -4,7 +4,9 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-start justify-center px-6 md:px-32 text-white bg-[url("/src/assets/heroImage.png")]
      bg-no-repeat bg-cover bg-center h-screen'>
-
+     <p>The Ultimate Hotel Experience</p>
+     <H1>Discover your perfect gateway destiination</H1>
+     <P>Unparalleled luxury and comfort awiat at the world's most exclusive hotels and resorts. Start your journey today.  </P>
     </div>
   )
 }
