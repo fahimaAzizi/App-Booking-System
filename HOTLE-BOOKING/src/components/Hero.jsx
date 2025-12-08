65304ce14e646ed1e586b9d3c810cc2dcf97bd9c
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-08 15:14:25 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/Hero.jsx
+++ HOTLE-BOOKING/src/components/Hero.jsx
@@ -35,9 +35,7 @@ const Hero = () => {
 
             <div>
                 <div className='flex items-center gap-2'>
-                    <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
-                    </svg>
+                 <img src={assets.calenderIcon} alt="" className="h-4" />
                     <label htmlFor="checkIn">Check in</label>
                 </div>
                 <input id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
@@ -45,9 +43,7 @@ const Hero = () => {
 
             <div>
                 <div className='flex items-center gap-2'>
-                    <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
-                    </svg>
+                   <img src={assets.calenderIcon} alt="" className="h-4" />
                     <label htmlFor="checkOut">Check out</label>
                 </div>
                 <input id="checkOut" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
@@ -59,9 +55,7 @@ const Hero = () => {
             </div>
 
             <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
-                <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
-                    <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
-                </svg>
+                <img src={assets.searchIcon alt="" className="h-4" />
                 <span>Search</span>
             </button>
         </form>
