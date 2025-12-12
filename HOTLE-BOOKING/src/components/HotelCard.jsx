aaaecc38aa08cdeb276d2ca4f067935cee35f5d7
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-12 23:21:34 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/HotelCard.jsx
+++ HOTLE-BOOKING/src/components/HotelCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const HotelCard = ({room , index}) => {
   return (
-    <Liink to={'/room' +room._id} onClick={()=> scrollTo(0.0)} key={room_id} >
+    <Link to={'/room' +room._id} onClick={()=> scrollTo(0.0)} key={room_id} >
       <image src ={room.images[0]} alt='' />
+      <p>Best seller</p>
       <div>
-        
+        <div>
+            <p className='font-playfair text-xl fontmedium text-gray-800'>{room.hotel.name}</p>
+            <div className='flex items-center gap-1'>
+                <img src={assets.starIconFilled} alt=" star-icon" />
+            </div>
+        </div>
+
       </div>
-    </Liink>
+    </Link>
   )
 }
  
