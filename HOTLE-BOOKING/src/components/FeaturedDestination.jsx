753c18951d0208669dcf9ba08c3f05e94bc7fe03
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-14 22:24:32 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/FeaturedDestination.jsx
+++ HOTLE-BOOKING/src/components/FeaturedDestination.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import HotelCard from './HotelCard'
+import { roomsDummyData } from '../assets/assets'
 
 const FeaturedDestination = () => {
   return (
-    <div>FeaturedDestination</div>
+    <div>
+      <div>
+        {roomsDummyData.slice(0,4).map((room , index)=>(
+          <HotelCard key={room._id}room={room}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
