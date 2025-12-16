0c69016dcd65aa7a69963242e1b02eeeb4955dc5
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-16 18:12:04 +0500

    Initial commit

---
+++ HOTLE-BOOKING/src/components/ExclusiveOffers.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { assets } from '../assets/assets';
+
+const ExclusiveOffers = ({ exclusiveOffers }) => {
+  return (
+    <div>
+      <div>
+        {exclusiveOffers.map((item) => (
+          <div
+            key={item._id}
+            className="group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center"
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            <p className="px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full">
+              {item.priceOff}% OFF
+            </p>
+
+            <div>
+              <p>{item.title}</p>
+              <p>{item.description}</p>
+              <p>Expires {item.expiryDate}</p>
+            </div>
+
+            <button>
+              View Offers
+              <img
+                className="invert group-hover:translate-x-1 transition-all"
+                src={assets.arrowIcon}
+                alt="arrow-icon"
+              />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ExclusiveOffers;
