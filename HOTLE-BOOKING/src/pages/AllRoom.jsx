a74cc681d11663398d8dab52c8a14509771bb899
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-21 15:14:26 +0500

    Initial commit

---
+++ HOTLE-BOOKING/src/pages/AllRoom.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AllRoom = () => {
+  return (
+    <div className='flex flex-col-reverse lg:flex-row itmes-start  justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+        <div>
+
+        </div>
+        {/* filters */}
+
+        <div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default AllRoom
