7ef07012f5a44392771dabbee942a65cd98d9706
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-21 15:18:48 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/AllRoom.jsx
+++ HOTLE-BOOKING/src/pages/AllRoom.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { roomsDummyData } from '../assets/assets'
 
 const AllRoom = () => {
   return (
     <div className='flex flex-col-reverse lg:flex-row itmes-start  justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
         <div>
+
+          {roomsDummyData.map((room)=(
+            <div>
+              <img src={room.images[0]} alt="hotel-img" title='Viw Room Detalis ' 
+              className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer' />
+            </div>
+          ))}
 
         </div>
         {/* filters */}
