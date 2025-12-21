b8342195d547fa9fbc0b21ba300bb230d8aba106
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-21 15:32:16 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/FeaturedDestination.jsx
+++ HOTLE-BOOKING/src/components/FeaturedDestination.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import HotelCard from './HotelCard'
 import { roomsDummyData } from '../assets/assets'
 import Title from './Title'
+import {userNavigate} from 'react-router-dom'
 
 const FeaturedDestination = () => {
+  const navigate = userNavigate()
   return (
     
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
@@ -15,6 +17,9 @@ const FeaturedDestination = () => {
           <HotelCard key={room._id}room={room} index={index}/>
         ))}
       </div>
+      <button onClick={()=>{navigate('/rooms'); scrollTo(0,0)}} className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>
+        View All Destinations
+      </button>
     </div>
   )
 }
