a990742a665e38750be48314991bff3c5dcd8b12
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-23 14:26:58 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/AllRoom.jsx
+++ HOTLE-BOOKING/src/pages/AllRoom.jsx
@@ -1,49 +1,63 @@
 import React from 'react'
 import { assets, roomsDummyData } from '../assets/assets'
 import StarRating from '../components/StarRating'
+import { useNavigate } from 'react-router-dom'
 
 const AllRoom = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div className='flex flex-col-reverse lg:flex-row itmes-start  justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
-        <div>
+    <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-36 px-4 md:px-16 lg:px-24 xl:px-32'>
 
-          {roomsDummyData.map((room)=>(
-            <div>
-              <img onClick={()=> {navigator(`/rooms/${rooms_id}`), scrollTo(0,0)}} src={room.images[0]} alt="hotel-img" title='Viw Room Detalis ' 
-              className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer' />
-              <div className='md:w-1/2 flex flex-col gap-2'>
-                <p className='text-gray-500'>
-                  {room.hotel.city}
-              
-                </p>
-                <p className='text-gary-800 text-3xl font-plyfair cursor-pointer'>{room.hotel.name}</p>
-                <div className='flex items-center'>
-                  <StarRating/>
-                  <p>200 + reviews</p>
-                </div>
+      {/* ROOMS */}
+      <div className='flex flex-col gap-10 w-full lg:w-2/3'>
+        {roomsDummyData.map((room) => (
+          <div key={room._id} className='flex flex-col md:flex-row gap-6'>
+
+            <img
+              onClick={() => {
+                navigate(`/rooms/${room._id}`)
+                window.scrollTo(0, 0)
+              }}
+              src={room.images[0]}
+              alt="hotel-img"
+              title='View Room Details'
+              className='max-h-64 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer'
+            />
+
+            <div className='md:w-1/2 flex flex-col gap-2'>
+              <p className='text-gray-500'>{room.hotel.city}</p>
+
+              <p className='text-gray-800 text-3xl font-playfair cursor-pointer'>
+                {room.hotel.name}
+              </p>
+
+              <div className='flex items-center gap-2'>
+                <StarRating />
+                <p>200+ reviews</p>
               </div>
-              <img src={assets.locationIcon} alt="location-icon" />
-              <span>{room.hotel.address}</span>
-            </div>
-          ))}
 
-        </div>
-        {/* filters */}
-        <div className='bg-white w-80 border-gray-300 text-gray-300 text-gray-600 
-        mmax-lg:mb-8 min-lg:mt-16'>
-          <div>
-            <p className='text-base font-medium text-gray-800'>FILTERS</p>
-            <div className='text-xs cursor-ponter'>
-              <span className="lg:hidden"> HIDE</span>
-              <span className='hidden lg:block'> CLEAR</span>
+              <div className='flex items-center gap-2 text-gray-600'>
+                <img src={assets.locationIcon} alt="location-icon" />
+                <span>{room.hotel.address}</span>
+              </div>
             </div>
+
           </div>
+        ))}
+      </div>
 
+      {/* FILTERS */}
+      <div className='bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 lg:mt-16 p-4'>
+        <div className='flex justify-between items-center'>
+          <p className='text-base font-medium text-gray-800'>FILTERS</p>
+          <div className='text-xs cursor-pointer'>
+            <span className="lg:hidden">HIDE</span>
+            <span className='hidden lg:block'>CLEAR</span>
+          </div>
         </div>
-
-        <div>
-            
-        </div>
+      </div>
 
     </div>
   )
