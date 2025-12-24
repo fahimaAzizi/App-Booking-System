3b7a19488ed91b36f0a6653742a54150a8230532
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-24 15:12:58 +0500

    Initial commit

--- HOTLE-BOOKING/src/App.jsx
+++ HOTLE-BOOKING/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer';
-import AllRoom from './pages/AllRoom';
+import AllRooms from './pages/AllRooms';
 
 
 const App = () => {
@@ -17,7 +17,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />} />  
-          <Route path='/rooms' element={<AllRoom/>}/>        
+          <Route path='/rooms' element={<AllRooms/>}/>        
         </Routes>
       </div>
       <Footer/>
