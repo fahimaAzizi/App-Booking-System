762c801c9f7a3cd802e0fd464fcd24b241f3b077
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-26 00:27:44 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/RoomDetails.jsx
+++ HOTLE-BOOKING/src/pages/RoomDetails.jsx
@@ -12,20 +12,35 @@ const RoomDetails = () => {
     room && setRoom(room);
     room && setMainImage(room.images[0]);
   }, [id]);
+return room && (
+  <div className="py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32">
+    
+    {/* Room Details */}
+    <div className="flex flex-col md:flex-row items-start md:items-center gap-2">
+      
+      <h1 className="text-3xl md:text-4xl font-playfair">
+        {room.hotel.name}{" "}
+        <span className="font-inter text-sm">
+          ({room.roomType})
+        </span>
+      </h1>
 
-  return (
-    room && (
-      <div className="py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32">
-        {/* Room Details */}
-        <div>
-          <h1 className="text-3xl md:text-4xl font-playfair">
-            {room.hotel.name} <span className="text-sm">({room.roomType})</span>
-          </h1>
-          <p className="text-red-500 font-medium mt-2">20% OFF</p>
-        </div>
-      </div>
-    )
-  );
+      <p className="text-xs font-inter py-1.5 px-3 text-white bg-orange-500 rounded-full">
+        20% OFF
+      </p>
+
+    </div>
+
+    {/* Room Rating */}
+    <div className="flex items-center gap-1 mt-2">
+      <StarRating />
+      <p className="ml-2">200+ reviews</p>
+    </div>
+
+  </div>
+)
+
+  
 };
 
 export default RoomDetails;
