176d78b3b507a2a9a16087ee90917744c7412df9
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-25 23:14:05 +0500

    Initial commit

---
+++ HOTLE-BOOKING/src/pages/RoomDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RoomDetails = () => {
+  const {id} = use
+  return (
+    <div>
+        <h1 className='pt-5'>room</h1>
+    </div>
+  )
+}
+
+export default RoomDetails
