986d9f89ca06bad57d9b6964b496ab7644601325
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-27 23:43:27 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/RoomDetails.jsx
+++ HOTLE-BOOKING/src/pages/RoomDetails.jsx
@@ -104,6 +104,15 @@ mx-auto mt-16 max-w-6xl gap-6">
     />
   </div>
 
+  {/* CHECK OUT */}
+  <div className="flex flex-col">
+    <label className="text-sm text-gray-600 mb-1">Check Out</label>
+    <input
+      type="date"
+      className="border border-gray-300 rounded-md px-3 py-2 outline-none"
+    />
+  </div>
+
 
   </div>
   
