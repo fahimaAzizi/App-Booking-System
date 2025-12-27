fc2f8d9b968f6abf43fedd4def0caf0b4f5f8a86
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-27 23:38:27 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/RoomDetails.jsx
+++ HOTLE-BOOKING/src/pages/RoomDetails.jsx
@@ -64,6 +64,32 @@ return room && (
   </div>
 
 </div>
+{/* Room Highlights */}
+<div className="flex flex-col md:flex-row md:justify-between mt-10">
+
+  <div className="flex flex-col">
+    <h1 className="text-3xl md:text-4xl font-playfair">
+      Experience Luxury Like Never Before
+    </h1>
+
+    <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">
+      {room.amenities.map((item, index) => (
+        <div
+          key={index}
+          className="flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100"
+        >
+          <img
+            src={facilityIcons[item]}
+            alt={item}
+            className="w-5 h-5"
+          />
+          <p className="text-xs">{item}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+
+</div>
 
 
   </div>
