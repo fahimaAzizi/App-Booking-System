c51165cbaf0f0bb193067231e30d2ed59eb4966a
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-29 15:34:44 +0500

    Initial commit

---
+++ HOTLE-BOOKING/src/pages/MyBookings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const MyBookings = () => {
+  return (
+    <div>
+        <Title Title='My Bookings' subTitle={}/>
+    </div>
+  )
+}
+
+export default MyBookings
