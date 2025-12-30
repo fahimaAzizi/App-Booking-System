0dc5046d3905b5c9f9934a3cfc28f7926bca4847
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-30 18:07:42 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/MyBookings.jsx
+++ HOTLE-BOOKING/src/pages/MyBookings.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import Title from '../components/Title'
 
 const MyBookings = () => {
-         <div>
+  return(
+         <div  className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
             <Title
         title="My Bookings"
         subTitle="Easily manage your past, current, and upcoming hotel reservations in one place. Plan your trips seamlessly with just a few clicks."
         align="left"
       />
+            {/* Table Wrapper */}
+      <div className="max-w-6xl mt-8 w-full text-gray-800">
+
+        {/* Table Header (Desktop only) */}
+        <div className="hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
+          <div>Hotels</div>
+          <div>Date & Timings</div>
+          <div>Payment</div>
+        </div>
     </div>
   )
 }
