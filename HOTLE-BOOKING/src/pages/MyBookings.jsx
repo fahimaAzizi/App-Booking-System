1310ce88a9cb8b18f58d7ef186d5468bc19b026a
Fahima Azizi <fahimaazizi397@gmail.com>
2025-12-31 16:45:11 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/MyBookings.jsx
+++ HOTLE-BOOKING/src/pages/MyBookings.jsx
@@ -61,11 +61,30 @@ const MyBookings = () => {
             </div>
 
             {/* ---- Payment ---- */}
-            <div className="flex items-center max-md:mt-4">
-              <span className="px-3 py-1 rounded-full bg-green-100 text-green-700 text-sm">
-                {booking.paymentStatus}
-              </span>
-            </div>
+        {/* ---- Payment Status ---- */}
+<div className="flex flex-col items-start justify-center pt-3">
+  <div className="flex items-center gap-2">
+    <div
+      className={`h-3 w-3 rounded-full ${
+        booking.isPaid ? "bg-green-500" : "bg-red-500"
+      }`}
+    ></div>
+
+    <p
+      className={`text-sm ${
+        booking.isPaid ? "text-green-500" : "text-red-500"
+      }`}
+    >
+      {booking.isPaid ? "Paid" : "Unpaid"}
+    </p>
+  </div>
+
+  {!booking.isPaid && (
+    <button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer">
+      Pay Now
+    </button>
+  )}
+</div>
 
           </div>
         ))}
