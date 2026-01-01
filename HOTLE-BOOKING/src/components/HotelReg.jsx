b8a6eaf91501aa9aac31b6438c684e882925fff4
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-01 22:51:05 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/HotelReg.jsx
+++ HOTLE-BOOKING/src/components/HotelReg.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const HotelReg = () => {
+      const HotelReg = ({ setShowHotelReg }) => {
   return (
-    <div>HotelReg</div>
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center bg-black/70">
+
+      {/* FORM CONTAINER */}
+      <form className="flex bg-white rounded-xl w-full max-w-4xl mx-4 overflow-hidden">
+
+        {/* LEFT IMAGE */}
+        <img
+          src={assets.regImage}
+          alt="reg-image"
+          className="w-1/2 hidden md:block object-cover"
+        />
+
+    </div>
   )
 }
 
