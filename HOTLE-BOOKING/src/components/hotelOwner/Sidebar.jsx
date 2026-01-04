cca21df2bdf11d52d49b4b0c987620fbf01e69b3
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-05 00:19:49 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/hotelOwner/Sidebar.jsx
+++ HOTLE-BOOKING/src/components/hotelOwner/Sidebar.jsx
@@ -8,6 +8,20 @@ const Sidebar = () => {
     { name: "Add Room", path: "/owner/add-room", icon: assets.addIcon },
     { name: "List Room", path: "/owner/list-room", icon: assets.listIcon },
   ];
+    return (
+    <div className="md:w-64 w-16 border-r h-full text-base border-gray-300 pt-4
+                    flex flex-col transition-all duration-300">
+
+      {sidebarLinks.map((item, index) => (
+        <NavLink
+          to={item.path}
+          key={index}
+          className={({ isActive }) =>
+            `flex items-center gap-3 px-4 py-2 hover:bg-gray-100 ${
+              isActive ? "bg-gray-200 font-semibold" : ""
+            }`
+          }
+        ></NavLink>
     </div>
   )
 }
