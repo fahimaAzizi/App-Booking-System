fc952fc7fab0d645fa8cca8914566df282feab1e
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-05 00:19:22 +0500

    Initial commit

--- HOTLE-BOOKING/src/components/hotelOwner/Sidebar.jsx
+++ HOTLE-BOOKING/src/components/hotelOwner/Sidebar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+        const sidebarLinks = [
+    { name: "Dashboard", path: "/owner", icon: assets.dashboardIcon },
+    { name: "Add Room", path: "/owner/add-room", icon: assets.addIcon },
+    { name: "List Room", path: "/owner/list-room", icon: assets.listIcon },
+  ];
+    </div>
   )
 }
 
