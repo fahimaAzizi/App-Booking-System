f838dbc486669655eb8c376ff67129b73934face
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-06 16:32:05 +0500

    Initial commit

--- HOTLE-BOOKING/src/App.jsx
+++ HOTLE-BOOKING/src/App.jsx
@@ -28,10 +28,11 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms/>}/>   
            <Route path='/rooms/:id' element={<RoomDetails/>}/>   
            <Route path='/my-bookings' element={<MyBookings/>}/>  
-            <Route path='/owner' element={<Layout/>}/>
+            <Route path='/owner' element={<Layout/>}>
             <Route index element={<Dashboard/>}/>
              <Route path='add-room'  element={<AddRoom/>}/>
              <Route path='list-room'  element={<ListRoom/>}/>
+           </Route>  
         </Routes>
       </div>
       <Footer/>
--- HOTLE-BOOKING/src/pages/hotelOwner/Dashboard.jsx
+++ HOTLE-BOOKING/src/pages/hotelOwner/Dashboard.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+   <div className="p-6">
+      {/* Page Title */}
+      <Title
+        align="left"
+        font="outfit"
+        title="Dashboard"
+        subTitle="Monitor your room listings, track bookings and analyze revenue—all in one place. Stay updated with real-time insights to ensure smooth operations."
+      />
   )
 }
 
