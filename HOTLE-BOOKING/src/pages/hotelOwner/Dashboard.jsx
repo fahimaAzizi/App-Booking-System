5872d88f395d1550ed51bbc51b1d84f817c3b60a
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-06 16:32:28 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/hotelOwner/Dashboard.jsx
+++ HOTLE-BOOKING/src/pages/hotelOwner/Dashboard.jsx
@@ -10,6 +10,21 @@ const Dashboard = () => {
         title="Dashboard"
         subTitle="Monitor your room listings, track bookings and analyze revenue—all in one place. Stay updated with real-time insights to ensure smooth operations."
       />
+      {/* Stats Cards */}
+      <div className="flex gap-4 my-8 flex-wrap">
+        
+        {/* Total Bookings */}
+        <div className="bg-primary/5 border border-primary/10 rounded flex items-center gap-4 p-4 pr-8">
+          <img
+            src={assets.totalBookingIcon}
+            alt="Bookings"
+            className="h-10 max-sm:hidden"
+          />
+          <div>
+            <p className="text-gray-500 text-sm">Total Bookings</p>
+            <p className="text-xl font-semibold">{totalBookings}</p>
+          </div>
+        </div>
   )
 }
 
