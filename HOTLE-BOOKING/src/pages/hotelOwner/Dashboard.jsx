9558babdb478ef632246958dd1d74803adb7fa31
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-09 20:41:07 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/hotelOwner/Dashboard.jsx
+++ HOTLE-BOOKING/src/pages/hotelOwner/Dashboard.jsx
@@ -1,10 +1,6 @@
-import React, { useState } from "react";
-import Title from "../components/Title";
-import { assets, dashboardDummyData } from "../assets/assets";
+import React from 'react'
 
-const Dashboard = () => {
-  const [dashboardData ,setDashboardData] = useState(dashboardDummyData);
-
+function Dashboard() {
   return (
     <div className="p-6">
       {/* Page Title */}
@@ -14,48 +10,10 @@ const Dashboard = () => {
         title="Dashboard"
         subTitle="Monitor your room listings, track bookings and analyze revenue—all in one place. Stay updated with real-time insights to ensure smooth operations."
       />
-
-      {/* Stats Cards */}
-      <div className="flex gap-4 my-8 ">
-
-        {/* Total Bookings */}
-        <div className="bg-primary/3 border border-primary/10 rounded flex items-center gap-4 p-4 pr-8">
-          <img
-            src={assets.totalBookingIcon}
-            alt="Bookings"
-            className="h-10 max-sm:hidden"
-          />
-          <div className="flex flex-col sm:ml-4 font-medium">
-            <p className="text-gray-500 text-sm">Total Bookings</p>
-            <p className="text-xl font-semibold">
-              {dashboardData.totalBookings}
-            </p>
-          </div>
-        </div>
-
-        {/* Total Revenue */}
-        <div className="bg-primary/5 border border-primary/10 rounded flex items-center gap-4 p-4 pr-8">
-          <img
-            src={assets.totalRevenueIcon}
-            alt="Revenue"
-            className="h-10 max-sm:hidden"
-          />
-          <div>
-            <p className="text-gray-500 text-sm">Total Revenue</p>
-            <p className="text-xl font-semibold">
-              $ {dashboardData.totalRevenue}
-            </p>
-          </div>
-        </div>
-
       </div>
-
       
+  )
+}
+<h1>jkfhgsdliug</h1>
 
-       
-      </div>
-    
-  );
-};
-
-export default Dashboard;
+export default Dashboard
--- HOTLE-BOOKING/src/pages/hotelOwner/Layout.jsx
+++ HOTLE-BOOKING/src/pages/hotelOwner/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/hotelOwner/Navbar'
 import Sidebar from '../../components/hotelOwner/Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
@@ -8,7 +9,8 @@ const Layout = () => {
       <Navbar/>
       <div className='flex h-full'>
         <Sidebar/>
-        <div className=''>
+        <div className=' flex-1 p-4 pt-10 md:px-10 h-full'>
+          <Outlet/>
 
         </div>
       </div>
