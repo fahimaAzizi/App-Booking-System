93b64bf7ec7178488c74668d0eb8c7a6a445cc93
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-10 17:57:36 +0500

    Initial commit

--- HOTLE-BOOKING/src/pages/hotelOwner/AddRoom.jsx
+++ HOTLE-BOOKING/src/pages/hotelOwner/AddRoom.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function AddRoom() {
+const AddRoom = () => {
+
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  });
+
+  const [inputs, setInputs] = useState({
+    roomType: "",
+    pricePerNight: 0,
+    amenities: {
+      "Free WiFi": false,
+      "Free Breakfast": false,
+      "Room Service": false,
+      "Mountain View": false,
+    },
+  });
+
   return (
-    <div>AddRoom</div>
-  )
-}
+    <div>
+      Add Room
+    </div>
+  );
+};
 
-export default AddRoom
+export default AddRoom;
