ad990b75f8c2def12a5a5aed3bcd0c50320724f3
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-16 16:25:08 +0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors())
 
-app.get('/',(req, res)=> res.send("API id wrking"))
+app.get('/',(req, res)=> res.send("API id working find"))
 
 const PORT = process.env.PORT || 3000;
 
