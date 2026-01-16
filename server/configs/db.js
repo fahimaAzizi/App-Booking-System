f1fd54a4a4964a6684250c5229acaf17ccab52a1
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-16 16:34:05 +0500

    Initial commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,1 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
+
+
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGOODB_URI}/hotel-booking`)
+    } catch (error) {
+        
+    }
+}
