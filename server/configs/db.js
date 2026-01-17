987fe2b3aa15269c4c885a846d784ae2c922e295
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-17 16:30:52 +0500

    Initial commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,8 +4,11 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
     try {
+        mongoose.connection.on('connected',()=> console.log("Database connected"))
         await mongoose.connect(`${process.env.MONGOODB_URI}/hotel-booking`)
     } catch (error) {
+        console.log(error.maessage);
         
     }
 }
+export default connectDB;
