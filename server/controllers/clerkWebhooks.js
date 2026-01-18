47607fd3e98003f964817abae7fe95f38e6067e8
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-18 14:42:39 +0500

    Initial commit

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -14,7 +14,28 @@ const clerkwebhooks = async (req , res)=>{
         await Whook.verify(JSON.stringify(req.body), headers)
 
         const {data , type}= req.body
-        
+
+        const userData = {
+  _id: data.id,
+  email: data.email_addresses[0].email_address,
+  username: data.first_name + " " + data.last_name,
+  image: data.image_url,
+};
+
+// Switch Cases for different Events
+switch (type) {
+  case "user.created": {
+    await User.create(userData);
+    break;
+  }
+
+  case "user.updated": {
+    await User.findByIdAndUpdate(data.id, userData);
+    break;
+  }
+}
+
+
     } catch (error) {
         
     }
