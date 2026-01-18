7bfd1bae54ad32c655e9afe2cba380c0f8e7f19b
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-18 14:40:46 +0500

    Initial commit

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -3,7 +3,18 @@ import { Webhook, Webhook } from "svix";
 
 const clerkwebhooks = async (req , res)=>{
     try {
-        const Webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const Whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        const hesders ={
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-tiemstamp"],
+            "svix-signature": req.headers["svix-signature"],
+        }
+
+        await Whook.verify(JSON.stringify(req.body), headers)
+
+        const {data , type}= req.body
+        
     } catch (error) {
         
     }
