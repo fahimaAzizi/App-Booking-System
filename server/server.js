5479f75a36ad5baa1a7494e1ecc8dd33da229c26
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-18 14:52:30 +0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import express from'express'
 import "dotenv/config";
 import cors from"cors";
-
+import { connectDB } from 'mongoose';
+import {clerkMiddleware} from '@clerk/express'
+import clerkwebhooks from './controllers/clerkWebhooks';
+connectDB()
 
 const app = express()
 
 app.use(cors())
+
+app.use(express.json())
+app.use(clerkMiddleware())
+
+// API
+
+app.use("/api/clerk",clerkwebhooks)
 
 app.get('/',(req, res)=> res.send("API id working find"))
 
