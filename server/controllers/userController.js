5268546379c7238e928fe2153a7cb5770efbb537
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-19 18:42:01 +0500

    Initial commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,26 @@
+// controllers/userController.js
+ export const getUserData = async (req , res)=>{
+    try {
+        const role = req.user.role;
+        const recentSearchedCities= req.user.recentSearchedCities;
+        res.json({success: true,role, recentSearchedCities})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+ }
+
+
 export const storeRecentSearchedCities = async (req, res) => {
   try {
     const { recentSearchedCity } = req.body;
     const user = req.user;
+
+    if (!recentSearchedCity) {
+      return res.status(400).json({
+        success: false,
+        message: "City is required",
+      });
+    }
 
     if (user.recentSearchedCities.length < 3) {
       user.recentSearchedCities.push(recentSearchedCity);
@@ -14,12 +33,12 @@ export const storeRecentSearchedCities = async (req, res) => {
 
     res.json({
       success: true,
-      message: "City added",
+      recentSearchedCities: user.recentSearchedCities,
     });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Something went wrong",
+      message: "Server Error",
     });
   }
 };
