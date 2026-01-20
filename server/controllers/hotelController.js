776a5097b4a2cbee1f79161588ea424650107196
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-20 23:42:10 +0500

    Initial commit

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -3,5 +3,16 @@ import User from "../models/User.js";
 
 
 export const registerHotel = async (req, res) =>{
-    
+    try {
+        const {name, address, contact , city} = req.body;
+        const owner = req.User._id
+
+       const hotel = await Hotel.findOne({owner})
+       if(hotel){
+        return res.json({success: false, message : "Hotel already Registered"})
+       }
+
+    } catch (error) {
+        
+    }
 }
