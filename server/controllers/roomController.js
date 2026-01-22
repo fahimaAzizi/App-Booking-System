7dfb272b0c0fb2fea2fedcb5d09e3f15731fd237
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-22 19:47:56 +0500

    Initial commit

---
+++ server/controllers/roomController.js
@@ -0,0 +1,9 @@
+
+// api to create a new room for a hotel 
+export  const create = async (req, res)=>{
+    try {
+        const {roomType, pricePerNight, amenities} = req.body;
+    } catch (error) {
+        
+    }
+}
