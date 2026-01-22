3407118b7fb400f7ec66e02b82bb373e19af5816
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-22 20:13:24 +0500

    Initial commit

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -3,6 +3,11 @@
 export  const create = async (req, res)=>{
     try {
         const {roomType, pricePerNight, amenities} = req.body;
+        const hotel = await Hotel.findOne({owner: req.auth.userId})
+
+        if(!hotel) return res.json({ success: false, message: "No Hotel Found"});
+
+        
     } catch (error) {
         
     }
