0c524014d4251562db66471f93b8e776e00b794a
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-26 16:27:00 +0500

    Initial commit

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema(
   {
+    user: {type: String, ref: " User", require: true},
+    room: {type: String, ref: " Room", require: true},
+    hotel: {type: String, ref: " Hotel", require: true},
+    chechInData: {type: Date, require: true},
+    chechOutData: {type: Date, require: true},
     
   },
   { timestamps: true }
