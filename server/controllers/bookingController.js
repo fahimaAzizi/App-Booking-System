41d8eddf6dabe8e2261eb517ecf4d3613b434cf4
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-28 21:36:47 +0500

    Initial commit

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,24 @@
+import Booking from "../models/Booking.js"
+
+// Function to Check Availability of Room
+const checkAvailability = async ({ checkInDate, checkOutDate, room }) => {
+  try {
+    const bookings = await Booking.find({
+      room,
+      checkInDate: { $lte: checkOutDate },
+      checkOutDate: { $gte: checkInDate },
+    });
+
+    const isAvailable = bookings.length === 0;
+    return isAvailable;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+
 // Check room availability API
-export const checkRoomAvailability = async (req, res) => {
+export const checkRoomAvailabilityAPI = async (req, res) => {
   try {
     const { room, checkInDate, checkOutDate } = req.body;
 
@@ -19,6 +38,13 @@ export const checkRoomAvailability = async (req, res) => {
 // POST /api/bookings/book
 export const createBooking = async (req, res) => {
   try {
+    const {room , checkInDate , checkOutDate, guests} = req.body;
+    const user = req.user._id;
+
+
+    const isAvailable = await checkAvailability({
+      
+    })
     // booking logic will be added here
   } catch (error) {
     res.json({ success: false, message: error.message });
