33a54db9ad61d34fca4052c468b514992161294e
Fahima Azizi <fahimaazizi397@gmail.com>
2026-01-30 19:22:38 +0500

    Initial commit

--- server/routes/bookingRouter.js
+++ server/routes/bookingRouter.js
@@ -1,6 +1,13 @@
 import express from 'express'
-import {cheAvailabilityAPI} from '../controllers/bookingController';
+import {cheAvailabilityAPI, createBooking, getHotelBookings, getUserBookings} from '../controllers/bookingController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const bookingRouter = express.Router();
 
 bookingRouter.post('/check-availability', cheAvailabilityAPI)
+bookingRouter.post('/book', protect,createBooking);
+bookingRouter.post('/user', protect, getUserBookings);
+bookingRouter.post('/hotel', protect, getHotelBookings);
+
+
+export default bookingRouter;
