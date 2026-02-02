113a571628baf1a557c5b223810a069ba7b75ae0
Fahima Azizi <fahimaazizi397@gmail.com>
2026-02-02 22:01:37 +0500

    Initial commit

--- HOTLE-BOOKING/src/App.jsx
+++ HOTLE-BOOKING/src/App.jsx
@@ -11,16 +11,19 @@ import Layout from './pages/hotelOwner/Layout';
 import Dashboard from './pages/hotelOwner/Dashboard';
 import AddRoom from './pages/hotelOwner/AddRoom';
 import ListRoom from './pages/hotelOwner/ListRoom';
-
+import { Toaster } from 'react-hot-toast';
+import { useAppContext } from './conext/AppContext';
 
 const App = () => {
   const location = useLocation();
   const isOwnerPath = location.pathname.includes("owner");
+  const {showHotelReg} = useAppContext();
 
   return (
     <div>
+      <Toaster />
       {!isOwnerPath && <Navbar />}
-     {false && <HotelReg />}
+     {showHotelReg && <HotelReg />}
 
       <div className='min-h-[70vh]'>
         <Routes>
